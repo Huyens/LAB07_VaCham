2828680afb284eccc6b0dc9e0421c1c56addfaf0
NgHuyen <154707638+Huyens@users.noreply.github.com>
2024-10-02 00:12:46 +0700

    Vacham

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    
+    public float moveSpeed = 5f;
+
+    void Update()
+    {
+        // Nhận đầu vào từ bàn phím
+        float moveHorizontal = Input.GetAxis("Horizontal"); // A/D hoặc Mũi tên trái/phải
+        float moveVertical = Input.GetAxis("Vertical"); // W/S hoặc Mũi tên lên/xuống
+
+        // Tạo vector di chuyển
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        // Di chuyển đối tượng
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
+    }
+}
